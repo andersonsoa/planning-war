3717eaa0778b77adcf2789ef32517bd392e25ec3
Anderson Soares <anderson.ceu@gmail.com>
2023-03-21 11:57:34 -0400

    cors

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from './modules/http/http.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), HttpModule],
 })
 export class AppModule {}
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
+  const configService = app.get(ConfigService);
+
+  const cors = configService.get('CORS_URL');
+
+  app.enableCors({ origin: cors });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
